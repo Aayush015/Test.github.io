4de4de585399a3d94178e7475bca7f3ba2e5f20e
Aayush Kafle <akafle1@unm.edu>
2023-12-18 18:17:44 -0700

    added new html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,18 @@
-function loader() {
-  document.querySelector('.loader-container').classList.add('fade-out');
-}
+document.addEventListener("DOMContentLoaded", function() {
+  function loader() {
+      document.querySelector('.loader-container').classList.add('fade-out');
+  }
 
-function fadeOut() {
-  // Set a timer for 3000 milliseconds (3 seconds) before calling loader
-  setTimeout(loader, 10000);
-}
+  function fadeOut() {
+      // Set a timer for 2000 milliseconds (2 seconds) before calling loader
+      setTimeout(loader, 2000);
+  }
 
-window.onload = fadeOut;
+  // Call fadeOut immediately after DOMContentLoaded
+  fadeOut();
+  
+  // Listen for the transitionend event to remove the loader after the fade-out transition is complete
+  document.querySelector('.loader-container').addEventListener('transitionend', function() {
+      document.querySelector('.loader-container').style.display = 'none';
+  });
+});
