745c0d10efd7b358f085b4d53351f8665caf903a
Aayush Kafle <aayush015@Aayushs-Mac-mini.local>
2024-12-29 22:03:43 -0700

    Added theme colors

---
+++ js/style-switcher.js
@@ -0,0 +1,28 @@
+/* ============================== Toggle Style Switcher ==============================*/
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    console.log("Toggle clicked");
+    document.querySelector(".style-switcher").classList.toggle("open");
+});
+// Hide style-switcher on scroll
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+});
+
+/* ============================== Theme Colors ==============================*/
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) 
+{
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else 
+        {
+            style.setAttribute("disabled", "true");
+        }
+    });
+}
