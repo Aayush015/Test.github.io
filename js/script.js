5e60b3871bfbabdbd303f48b222fa5876667e8f9
Aayush Kafle <aayush015@Aayushs-Mac-mini.local>
2024-12-30 07:53:41 -0700

    Nav bar can navigate to different pages

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================= Typing Animation ========================= */
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Web Developer", "App Developer", "Soccer player"],
+    typeSpeed: 100,
+    BackSpeed: 60, 
+    loop: true
+})
