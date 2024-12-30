80d33e15b57356834403a408279bb46dc1fc427a
Aayush Kafle <aayush015@Aayushs-Mac-mini.local>
2024-12-30 08:51:45 -0700

    Respective Navbar activates as they are clicked

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ var typed = new Typed(".typing", {
     BackSpeed: 60, 
     loop: true
 })
+/* ========================= Aside Navigation ========================= */
+const nav = document.querySelector(".nav");
+const navLinks = nav.querySelectorAll("a"); // Select all navigation links
+
+navLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+        // Remove "active" class from all navigation links
+        navLinks.forEach((navLink) => navLink.classList.remove("active"));
+
+        // Add "active" class to the clicked link
+        this.classList.add("active");
+    });
+});
